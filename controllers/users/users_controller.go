f322abb1c20f53908dc5ee59852d1f73060f98d2
lordradamanthys <mateuslima565@gmail.com>
2021-12-31 00:12:11 -0300

    config router struct, parse json to struct and response, created a error utils

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,11 +3,27 @@ package users
 import (
 	"net/http"
 
+	"github.com/LordRadamanthys/bookstore_oauth-api/domain/users"
+	"github.com/LordRadamanthys/bookstore_oauth-api/services"
+	"github.com/LordRadamanthys/bookstore_oauth-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.BadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
